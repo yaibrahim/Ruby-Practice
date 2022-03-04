4331643ec208425a391e5c494b61018c80084ff6
Hasan <hasanadeym@gmail.com>
2022-03-04 16:29:24 +0500

    Add Practice Task 1

--- Practice Tasks/practice1.rb
+++ Practice Tasks/practice1.rb
@@ -105,12 +105,6 @@ text.each_char { |char| freqs[char] += 1 }
 # print freqs
 ('a'..'z').each { |x| puts "#{x} : #{freqs[x]}" }
 
-# Number Frequency counter (Practice Codes)
-arr3 = [1, 2, 3, 4, 1, 1, 1, 2, 3, 11, 11, 2, 33]
-print arr3
-freqs1 = {}
-freqs1.default = 0
-arr3.each { |c| freqs1[c] += 1 }
-puts freqs1
+
 
 
