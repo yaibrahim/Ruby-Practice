097dcb7d41c56cd9e6b8bd38c982769ca1e69d52
Hasan <hasanadeym@gmail.com>
2022-03-08 13:53:37 +0500

    Task 3 Done

--- Practice Tasks/Task3.rb
+++ Practice Tasks/Task3.rb
@@ -1,7 +1,11 @@
+puts "Enter Desired Length of password"
 n = gets.to_i
-alpha_length = 10
-num_length = 10
-symbols_length = 14
+puts "Enter Length of alphabets less than or equal to 26"
+alpha_length = gets.to_i
+puts "Enter Length of numerical less than or equal to 10"
+num_length = gets.to_i
+puts "Enter Length of symbols less than or equal to 20"
+symbols_length = gets.to_i
 
 numeric = "0123456789"
 alphabets_small = "abcdefghijklmnopqrstuvwxyz"
@@ -14,52 +18,52 @@ if alpha_length <= alphabets_large.length() && alpha_length <= alphabets_small.l
 
   #puts numeric[0,num_length] + alphabets_small[0,alpha_length] + alphabets_large[0,alpha_length] +symbols[0,symbols_length]
   passItems = numeric[0,num_length] + alphabets_small[0,alpha_length] + alphabets_large[0,alpha_length] +symbols[0,symbols_length]
+  password = ""
+  i=0
 
-else
-  puts "Check your input of numeric , alphabet and sysmbols length...
-  \n alphabets actual length is 26  \n Numerics are 10 \n symbols are 20"
-end
-
-password = ""
-i=0
-
-if n >= 7 && passItems.length() > 0
-  while (i < n)
+  if n >= 7 && n <= passItems.length()
+   while (i < n)
     rand_value = rand 0..passItems.length()-1
     # puts rand_value
     password += passItems[rand_value]
     i = i + 1
-  end
+   end
 
-  puts "Random Generated Password: "
-  if(password.length() > 0)
-    puts password
-  else
-    puts "Unable to generate password"
-  end
+   puts "Random Generated Password: "
+   if(password.length() > 0)
+     puts password
+   else
+     puts "Unable to generate password"
+   end
 
-  flag = false
-  File.readlines("passwords1.csv").each {
-     |line|
-     #puts " --- #{line}"
-     if password == line
-       flag = true
-     else
-       flag = false
-       #puts "no found in record"
-     end
+   flag = false
+   File.readlines("passwords1.csv").each {
+      |line|
+      #puts " --- #{line}"
+      if password == line
+        flag = true
+      else
+        flag = false
+        #puts "no found in record"
+      end
     }
 
-  if flag == false
-    pass_file = File.new("passwords1.csv", "a")
-    pass_file.puts(password)
-    pass_file.close
+   if flag == false
+     pass_file = File.new("passwords1.csv", "a")
+     pass_file.puts(password)
+     pass_file.close
 
-    puts "Password Generated And save in file successfully"
-    #read_file = File.read("passwords1.csv")
-    #puts read_file
-  else
-    puts "Password already exist in record!!"
-  end
+     puts "Password Generated And save in file successfully"
+     #read_file = File.read("passwords1.csv")
+     #puts read_file
+   else
+     puts "Password already exist in record!!"
+   end
+ else
+   puts "The Length of Password should be greater than equal to 7"
+ end
 
+else
+   puts "Check your input of numeric , alphabet and sysmbols length...
+   \n alphabets actual length is 26  \n Numerics are 10 \n symbols are 20"
 end
